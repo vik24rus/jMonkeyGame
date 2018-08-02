dd6e08d6ad7fe7aac274b8160f28e6129f0df2a0
vik24rus <vik24rus@gmail.com>
2018-08-03 00:18:19 +0700

    add all for javafx (FXML,view), testing events

--- src/main/java/UI/UIplayerInfoController.java
+++ src/main/java/UI/UIplayerInfoController.java
@@ -1,4 +1,29 @@
 package UI;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import javafx.stage.WindowEvent;
+
+import java.io.IOException;
+
 public class UIplayerInfoController {
+    @FXML
+    private Button buttonClose;
+
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    public void actionButtonClose() {
+        Stage stage = (Stage) buttonClose.getScene().getWindow();
+        stage.close();
+    }
 }
